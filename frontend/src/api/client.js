c2322caa5c0e70286823b95d73cc24db46e5b8bc
praful <prafuldkasamalagi2353@gmail.com>
2026-02-28 02:26:14 +0530

    chore: point api client to production render url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -5,7 +5,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://dreamalpha-project-navonmesh.onrender.com',
     timeout: 10000,
 })
 
